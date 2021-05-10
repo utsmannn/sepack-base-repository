666e935431f657a2dca91379ddeec93076ceb18f
utsmannn <kucingapes@gmail.com>
2021-05-10 23:45:41 +0700

    first commit

---
+++ app/src/main/java/com/sepack/basic/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sepack.basic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
